62fc99f0f12b4d2c65a92d16d89eaf42fb662868
Vadim <vsuffering@gmail.com>
2022-09-29 22:56:33 +1000

    add hint 1

--- Example001_HelloConsole/Program.cs
+++ Example001_HelloConsole/Program.cs
@@ -1,1 +1,1 @@
-﻿Console.WriteLine("2Hello, World!");
+﻿Console.WriteLine("2Hello, World!");//Console - контейнер (хранение переменной), WriteLine - в конце перейти на новую строку.
--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.Write("Введите ваше имя ");
-string username = Console.ReadLine();
+﻿Console.Write("Введите ваше имя ");// Write - Вывод в одну строку
+string username = Console.ReadLine();// string -строка, Readline - Считать строку из терминала.
 Console.Write("Привет, ");
 Console.Write(username);
