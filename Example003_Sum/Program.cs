c03bff25720cc795048e48c55a4193b9e0e750a0
Vadim <vsuffering@gmail.com>
2022-09-29 23:14:12 +1000

    change Example 003 Sum plus hint

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3;//int - целое число.
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10);//int - целое число. 1,2,3,4 ...9
+Console.WriteLine(numberA);// строка, чтобы выводить числа случайные, а не гадать какие были загаданы.
+int numberB = new Random().Next(1,10);//new Random ().Next() - случайное число от() и до().
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
